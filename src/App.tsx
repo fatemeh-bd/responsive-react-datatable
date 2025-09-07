02db2c4d592757d75b3e3c42384ba176c86ef237
Zephinax <zephinax@gmail.com>
2025-09-07 13:31:50 +0330

    add app and remove main button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Table from "./components/table/Table";
+import { ColumnType } from "./components/table/types";
 
 const App: React.FC = () => {
-  return <div className="min-h-screen w-screen  p-6 bg-black">شسیشسیشیش</div>;
+  const columns: ColumnType[] = [
+    {
+      data: "contractName",
+      orderable: true,
+      title: "نام قرارداد",
+      searchable: false,
+      width: 200,
+    },
+    {
+      data: "startDate",
+      orderable: true,
+      title: "تاریخ شروع",
+      searchable: false,
+      width: 140,
+    },
+    {
+      data: "endDate",
+      orderable: true,
+      title: "تاریخ پایان",
+      searchable: false,
+      width: 140,
+    },
+    {
+      data: "createdAt",
+      orderable: true,
+      title: "تاریخ ایجاد",
+      searchable: false,
+      width: 140,
+    },
+  ];
+  return (
+    <div className="min-h-screen w-screen  p-6 bg-black">
+      <Table columns={columns} endpoint="endpoint" title="title" />
+    </div>
+  );
 };
 
 export default App;
