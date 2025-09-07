55c692600662bbc2b26b30df11e09e4a329b7b99
Zephinax <zephinax@gmail.com>
2025-09-07 12:23:38 +0330

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ReactPictureSelector/",
+  base: "/responsive-react-datatable/",
   root: ".",
   server: {
     port: 5173,
