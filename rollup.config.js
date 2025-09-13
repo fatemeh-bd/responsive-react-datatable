b8e03db52da9e6d5ff77bb46701f30712475532e
Zephinax <zephinax@gmail.com>
2025-09-13 13:20:28 +0330

    prerelase

--- rollup.config.js
+++ rollup.config.js
@@ -14,11 +14,6 @@ export default [
     input: "./src/index.ts",
     output: [
       {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: false,
-      },
-      {
         file: packageJson.module,
         format: "esm",
         sourcemap: false,
