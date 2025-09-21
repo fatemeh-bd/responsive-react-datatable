5e586e32017ef382957e2791ccea0981e690b8a9
Zephinax <zephinax@gmail.com>
2025-09-21 16:12:23 +0330

    update some styles

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ const App: React.FC = () => {
                 </svg>
               </div>
             </div>
-            <button className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors">
+            <button className="px-4 py-2 text-sm font-medium text-white min-w-fit bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors">
               اعمال فیلتر
             </button>
           </div>
