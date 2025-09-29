9e1eee78e71b5b311ff59b4343166b64321bee49
fatemeh <baridaryan@gmail.com>
2025-09-29 14:34:44 +0330

    add: export CustomBody Type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Table } from "./components/table/Table";
 export { default } from "./components/table/Table";
-export type { ColumnType } from "./components/table/types";
+export type { ColumnType, CustomBody } from "./components/table/types";
 export { rowRenderer } from "./components/table/helper";
