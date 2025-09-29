dbe0f2179fcc7aed7ee360770f9a604fdaf03985
fatemeh <baridaryan@gmail.com>
2025-09-29 14:36:53 +0330

    add: export AutoPageSizeConfig Type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { default as Table } from "./components/table/Table";
 export { default } from "./components/table/Table";
-export type { ColumnType, CustomBody } from "./components/table/types";
+export type {
+  ColumnType,
+  CustomBody,
+  AutoPageSizeConfig,
+} from "./components/table/types";
 export { rowRenderer } from "./components/table/helper";
