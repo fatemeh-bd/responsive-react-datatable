da6e8b46ccc732182dacfddca3507819f2491670
fatemeh <baridaryan@gmail.com>
2025-10-04 11:05:47 +0330

    update: remove filters from optionalSelectorsForExtraBuffer

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -143,7 +143,7 @@ const Table: React.FC<TableProps> = (props) => {
     ],
     optionalSelectorsForExtraBuffer: [
       "#topFilter",
-      "#filters",
+      // "#paging",
       ...((customAutoPageSizeConfig?.optionalSelectorsForExtraBuffer as string[]) ||
         []),
     ],
@@ -520,7 +520,6 @@ const Table: React.FC<TableProps> = (props) => {
           )}
           {!isMobile && filters && (
             <div
-              id="filters"
               className={`${filterContainerClassName} filter-container-className`}
             >
               {filters}
