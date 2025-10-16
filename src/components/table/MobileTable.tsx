5afe9ae8af5183eedca21ed3749716c20e13f4b0
Zephinax <zephinax@gmail.com>
2025-10-16 16:16:09 +0330

    feat: add memo to mobile table

--- src/components/table/MobileTable.tsx
+++ src/components/table/MobileTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { memo, useMemo } from "react";
 import { ColorTheme, ColumnType, TextsConfig } from "./types";
 import Skeleton from "./tools/Skeleton";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -6,6 +6,77 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Virtual } from "swiper/modules";
+
+// کامپوننت کارت جداگانه با memo
+const TableCard = memo(
+  ({
+    row,
+    rowIndex,
+    columns,
+    theme,
+  }: {
+    row: Record<string, any>;
+    rowIndex: number;
+    columns: ColumnType[];
+    theme: ColorTheme;
+  }) => {
+    return (
+      <div
+        style={{
+          background: theme?.backgroundColor,
+          borderColor: theme?.borderColor,
+        }}
+        className="mobile-table-card shadowSmall"
+      >
+        {columns.map((column, colIndex) => {
+          const cellKey = column.data;
+          // استفاده از useMemo برای بهینه‌سازی رندر محتوا
+          const content = useMemo(
+            () =>
+              column?.render
+                ? column.render(
+                    cellKey ? row[cellKey] : undefined,
+                    row,
+                    rowIndex,
+                  )
+                : cellKey
+                  ? row[cellKey]
+                  : null,
+            [column, row, cellKey, rowIndex],
+          );
+
+          return column?.dontShowDataInMobile ? null : (
+            <div key={colIndex} className="mobile-table-card-row">
+              {column?.title === "عملیات" ||
+              column?.data === null ||
+              column?.dontShowTitleInMobile ? null : (
+                <span
+                  style={{ color: theme?.headerTextColor }}
+                  className="mobile-table-card-label"
+                >
+                  {column?.title}
+                  {column?.title && ":"}
+                </span>
+              )}
+              <div
+                title={cellKey ? row[cellKey]?.toString() : undefined}
+                className={`mobile-table-card-content ${
+                  column?.title === "عملیات" || column?.data === null
+                    ? "isOperation"
+                    : ""
+                }`}
+              >
+                {content}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  },
+);
+
+TableCard.displayName = "TableCard";
 
 const MobileTable = ({
   columns,
@@ -22,92 +93,47 @@ const MobileTable = ({
   textsConfig: TextsConfig;
   listMode?: boolean;
 }) => {
-  const renderCard = (row: Record<string, any>, rowIndex: number) => (
-    <div
-      key={rowIndex}
-      style={{
-        background: theme?.backgroundColor,
-        borderColor: theme?.borderColor,
-      }}
-      className="mobile-table-card shadowSmall"
-    >
-      {columns.map((column, colIndex) => {
-        const cellKey = column.data;
-        const content = column?.render
-          ? column?.render(cellKey ? row[cellKey] : undefined, row, rowIndex)
-          : cellKey
-          ? row[cellKey]
-          : null;
-
-        return column?.dontShowDataInMobile ? null : (
-          <div key={colIndex} className="mobile-table-card-row">
-            {column?.title === "عملیات" ||
-            column?.data === null ||
-            column?.dontShowTitleInMobile ? null : (
-              <span
-                style={{ color: theme?.headerTextColor }}
-                className="mobile-table-card-label"
-              >
-                {column?.title}
-                {column?.title && ":"}
-              </span>
-            )}
-
-            <div
-              title={cellKey ? row[cellKey]?.toString() : undefined}
-              className={`mobile-table-card-content ${
-                column?.title === "عملیات" || column?.data === null
-                  ? "isOperation"
-                  : ""
-              }`}
-            >
-              {content}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-
   return (
     <div className="mobile-table-container">
       {isLoading ? (
         <div className="mobile-table-skeleton-container space-items">
-          {columns?.map((_item, index) => (
-            <React.Fragment key={index}>
-              <div className="mobile-table-skeleton-row">
-                <Skeleton
-                  className="mobile-table-skeleton-item"
-                  style={{ width: index % 2 ? "40%" : "60%", margin: 0 }}
-                  height="20px"
-                />
-                <Skeleton
-                  className="mobile-table-skeleton-item"
-                  style={{ width: index % 2 ? "60%" : "40%", margin: 0 }}
-                  height="20px"
-                />
-              </div>
-              {_item?.data === null && (
-                <div className="mobile-table-skeleton-action-row">
-                  <Skeleton
-                    className="mobile-table-skeleton-action"
-                    height="36px"
-                    style={{ width: "50%", margin: 0 }}
-                  />
-                  <Skeleton
-                    className="mobile-table-skeleton-action"
-                    height="36px"
-                    style={{ width: "50%", margin: 0 }}
-                  />
-                </div>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <div key={index} className="mobile-table-skeleton-card shadowSmall">
+              {columns.map((column, colIndex) =>
+                column?.dontShowDataInMobile ? null : (
+                  <div key={colIndex} className="mobile-table-skeleton-row">
+                    {column?.title === "عملیات" ||
+                    column?.data === null ||
+                    column?.dontShowTitleInMobile ? null : (
+                      <Skeleton
+                        className="mobile-table-skeleton-item"
+                        style={{ width: "40%", margin: 0 }}
+                        height="20px"
+                      />
+                    )}
+                    <Skeleton
+                      className="mobile-table-skeleton-item"
+                      style={{ width: "60%", margin: 0 }}
+                      height="20px"
+                    />
+                  </div>
+                ),
               )}
-            </React.Fragment>
+            </div>
           ))}
         </div>
       ) : rows?.length > 0 ? (
         listMode ? (
           <div className="mobile-table-list-container">
-            {rows.map((row, rowIndex) => renderCard(row, rowIndex))}
+            {rows.map((row, rowIndex) => (
+              <TableCard
+                key={row.id || rowIndex} // ترجیحاً از row.id استفاده کنید
+                row={row}
+                rowIndex={rowIndex}
+                columns={columns}
+                theme={theme}
+              />
+            ))}
           </div>
         ) : (
           <Swiper
@@ -118,8 +144,16 @@ const MobileTable = ({
             virtual
           >
             {rows.map((row, rowIndex) => (
-              <SwiperSlide key={rowIndex} className="mobile-table-swiper-slide">
-                {renderCard(row, rowIndex)}
+              <SwiperSlide
+                key={row.id || rowIndex}
+                className="mobile-table-swiper-slide"
+              >
+                <TableCard
+                  row={row}
+                  rowIndex={rowIndex}
+                  columns={columns}
+                  theme={theme}
+                />
               </SwiperSlide>
             ))}
           </Swiper>
